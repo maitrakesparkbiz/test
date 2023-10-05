9b7f26f8fd307ca4591fa8d50a2a919e122115b9
Maitrak_esbz <nirmal.maitrak@esparkbizmail.com>
2023-10-05 11:29:47 +0530

    init

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (message) => {
+    console.log("Received message:", message);
+    // Store the message or process it as needed
+
+    // Broadcast the message to all connected clients
+    io.emit("message", message);
+  });
+
+  socket.on("activeUser", (message) => {
+    console.log("activeUser", message);
+    // Store the message or process it as needed
+
+    // Broadcast the message to all connected clients
+    io.emit("activeUser", message);
+  });
+
+  socket.on("inactiveUser", (message) => {
+    console.log("inactiveUser", message);
+    // Store the message or process it as needed
+
+    // Broadcast the message to all connected clients
+    io.emit("inactiveUser", message);
+  });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
